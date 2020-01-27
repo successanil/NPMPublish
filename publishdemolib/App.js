497b09e47d1e297272c4605347247c3bf7be7ea1
Relsell <relsellglobal@gmail.com>
2020-01-27 18:07:01 +0530

    udpate code for 007

--- publishdemolib/App.js
+++ publishdemolib/App.js
@@ -7,34 +7,72 @@
  */
 
 import React from 'react';
-import {View} from 'react-native';
+import {View, TextInput, Button,Text} from 'react-native';
 import {Component} from 'react';
-
 const app = require('rg-weather-app-node');
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       props: props,
+      temperature:'00.00',
     };
+    this.inputCity = '';
   }
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
 
   render() {
-    return <View></View>;
+    return (
+      <View style={{flex: 1}}>
+        <View
+          style={{flex: 1, paddingLeft: 16, paddingRight: 16, paddingTop: 16}}>
+          <TextInput
+            style={{borderWidth: 1}}
+            placeholder="Enter city name"
+            editable
+            onChangeText={text => this.inputCity = text}
+            maxLength={40}
+          />
+          <View style={{marginTop: 16}}></View>
+          <Button
+            title="Fetch temperature"
+            color="#f194ff"
+            onPress={() => {
+              var argvL = this.state.props.argv;
+              argvL.a = this.inputCity;
+              this.startProcessForTemp(argvL);
+            }}
+          />
+        </View>         
+         <View style={{flex: 1, borderWidth: 1,justifyContent:'center',alignItems:'center'}}>
+          <Text>Temperature in input city is {this.state.temperature} Fahrenheit</Text>
+        </View>
+      </View>
+    );
+  }
+  startProcessForTemp(argv) {
+    // console.log('Test params');
+    // console.log(argv);
+    var p = app.getGeoCoding(argv);
+    p
+    .then((result)=>{
+      return app.getWeather(argv,result);
+    })
+    .then((result)=>{
+      this.setState({
+            temperature:result.tempreture
+          });
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+    
+
+    // app.startProcess(argv, result => {
+    //   this.setState({
+    //     temperature:10
+    //   });
+    // });
   }
 }
-
-var startProcessForTemp = (argv) => {
-  console.log('Test params');
-  console.log(argv);
-  app.startProcess(argv);
-}
-
-module.exports = {
-  App,
-  startProcessForTemp
-};
--- publishdemolib/index.js
+++ publishdemolib/index.js
@@ -3,9 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import {App,startProcessForTemp} from './App';
+import AppWrapper from './AppWrapper';
 import {name as appName} from './app.json';
 
 
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppWrapper);
