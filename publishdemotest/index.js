959d68c6e14f96ea1091224ef23cdb804db19d67
Relsell <relsellglobal@gmail.com>
2020-01-27 08:55:22 +0530

    added publishdemotest rn app

---
+++ publishdemotest/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from 'publishdemolib';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
