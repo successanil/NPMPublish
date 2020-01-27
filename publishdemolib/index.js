7845f2c8b5fa0a042619f96e52eca5d38e6f03c8
Relsell <relsellglobal@gmail.com>
2020-01-27 12:22:58 +0530

    update

--- publishdemolib/index.js
+++ publishdemolib/index.js
@@ -7,13 +7,5 @@ import {App,startProcessForTemp} from './App';
 import {name as appName} from './app.json';
 
 
-// var argv = {
-//     a: 'New Delhi',
-//     gapi_key: localconfig.GMAP_KEY,
-//     dapi_key: localconfig.DARK_SKY_API_KEY,
-//   };
-
-//startProcessForTemp(argv);
-
 
 AppRegistry.registerComponent(appName, () => App);
